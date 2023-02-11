0c8fcba768ff5631486baea09125df1ef9b4460b
kinumiya-norihito <99883931+kinumiya-norihito@users.noreply.github.com>
2023-02-12 01:09:10 +0900

    make simple ui

--- calculator-for-iPad.swiftpm/Package.swift
+++ calculator-for-iPad.swiftpm/Package.swift
@@ -17,7 +17,6 @@ let package = Package(
             name: "calculator-for-iPad",
             targets: ["AppModule"],
             bundleIdentifier: "com.KC3-2023-C.calculator-for-iPad",
-            teamIdentifier: "",
             displayVersion: "1.0",
             bundleVersion: "1",
             appIcon: .placeholder(icon: .pencil),
