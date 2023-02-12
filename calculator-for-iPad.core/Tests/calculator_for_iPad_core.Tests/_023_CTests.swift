ada00947ba2baae1d8e4e045e7547fccd2bb7e13
yu195 <miyayuu.since2018@gmail.com>
2023-02-12 17:17:00 +0900

    プロジェクト名を変更

--- calculator-for-iPad.core/Tests/calculator_for_iPad_core.Tests/_023_CTests.swift
+++ calculator-for-iPad.core/Tests/calculator_for_iPad_core.Tests/_023_CTests.swift
@@ -1,11 +1,12 @@
 import XCTest
-@testable import _023_C
+@testable import calculator_for_iPad_core
 
 final class _023_CTests: XCTestCase {
     func testExample() throws {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(_023_C().text, "Hello, World!")
+        var temp = _023_C();
+        XCTAssertEqual(temp.text, "Hello, World!")
     }
 }
