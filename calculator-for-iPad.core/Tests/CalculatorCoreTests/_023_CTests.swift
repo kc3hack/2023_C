382d3857a2cf4f82e412f7e14ff974a7a380885c
TAN-21-den <103180334+TAN-21-den@users.noreply.github.com>
2023-02-13 21:29:53 +0900

    コンフリクト修正中

--- calculator-for-iPad.core/Tests/CalculatorCoreTests/_023_CTests.swift
+++ calculator-for-iPad.core/Tests/CalculatorCoreTests/_023_CTests.swift
@@ -1,13 +1,18 @@
 import XCTest
-@testable import _023_C
+@testable import CalculatorCore
 
 final class _023_CTests: XCTestCase {
     func testExample() throws {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
+<<<<<<< HEAD:calculator-for-iPad.core/Tests/2023_CTests/_023_CTests.swift
         XCTAssertEqual(_023_C().text, "Hello, World!")
 
         
+=======
+        var temp = _023_C();
+        XCTAssertEqual(temp.text, "Hello, World!")
+>>>>>>> main:calculator-for-iPad.core/Tests/CalculatorCoreTests/_023_CTests.swift
     }
 }
