6cf0bd614446b7dbd904b0c7e69bf2ffb6062c53
sacradius <s256255@wakayama-u.ac.jp>
2023-02-17 16:12:47 +0900

    数字と単項演算子をトークンに切り分ける部分

--- calculator-for-iPad.core/Sources/CalculatorCore/Services/CalculatorService.swift
+++ calculator-for-iPad.core/Sources/CalculatorCore/Services/CalculatorService.swift
@@ -8,6 +8,52 @@ public class CalculatorService: PCalculatorService {
         return Expression(rawExpression: ["[String]"], tokens: [NanValue()])
     }
 
+    private func parseString(rawExpression: String) -> [Token]? {
+        var token: String = ""
+        var tokenList: [String] = []
+        // 0: 受付中
+        // 1: 数字を認識中
+        // 2: 演算子を認識中
+        var state = 0
+        for c: Character in rawExpression {
+            if c.isNumber || c == "." {
+                // state 2 ならパース失敗
+                // それ以外ならtokenに突っ込む
+                // stateを1にする
+                if(state == 2) {
+                    return nil
+                }else {
+                    token += String(c)
+                    state = 1
+                }
+            }else {
+                // state 1 => 数字に変換 失敗 nil
+                // 受け取った文字列 => 演算子に変換できるか  => 変換できたらstate 0 tokenを""
+                // state2 => tokenと合わせて変換
+                // 変換できなかったらtokenに入れてstate2にする
+                if(state == 1) { //トークンに数字が入っていたらここで吐き出し
+                    guard (Double(token)!.truncatingRemainder(dividingBy: 1) >= 0) else { //数値であるか調べる
+                        return nil
+                    }
+                    tokenList.append(token)
+                    token = ""
+                    state = 0
+                }
+
+                if(c == "+" && c == "-" && c == "*" && c == "/" && c == "^" && c == "√"/* && c == "(" && c == ")"*/) { //単項演算子(と括弧)用
+                    tokenList.append(token)
+                    token = ""
+                    state = 0
+                }
+            }
+        }
+        if(token != "") { //末尾のトークンを吐き出し
+            tokenList.append(token)
+            token = ""
+            state = 0
+        }
+    }
+
     private func infix2polish(expression: [Token]) -> [Token]? {
         var stack: [Operator] = []
         var result: [Token] = []
