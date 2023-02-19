f17aed8748309cd7a1aa93355e71ef723e0716e0
kinumiya-norihito <99883931+kinumiya-norihito@users.noreply.github.com>
2023-02-19 09:29:54 +0900

    UIの向上、ContentViewの一部をControllerViewに先送り

--- calculator-for-iPad.swiftpm/OtherKeys.swift
+++ calculator-for-iPad.swiftpm/OtherKeys.swift
@@ -21,31 +21,27 @@ struct OtherKeys: View {
     ]
     
     var body: some View {
-        //ScrollView() {
-        VStack{
-            ForEach((0..<key_array.count),id: \.self){
-                index_r in
-                let main_key_array_row = key_array[index_r]
-                HStack {
-                    ForEach((0..<main_key_array_row.count),id: \.self){
-                        index_c in
-                        let key_char = main_key_array_row[index_c]
-                        // ボタンの生成
-                        Button(action: {()->Void in
-                            onckick(push_char: key_char)
-                        }){
-                            Text(key_char)
-                                .font(.system(size: font_size))
-                                .frame(maxWidth: .infinity, maxHeight: .infinity)
-                        }
-                        .background(Color(red:0.8, green:0.9, blue:0.95))
-                        .foregroundColor(Color.black)
-                            .cornerRadius(10)
+        ForEach((0..<key_array.count),id: \.self){
+            index_r in
+            let main_key_array_row = key_array[index_r]
+            HStack {
+                ForEach((0..<main_key_array_row.count),id: \.self){
+                    index_c in
+                    let key_char = main_key_array_row[index_c]
+                    // ボタンの生成
+                    Button(action: {()->Void in
+                        onckick(push_char: key_char)
+                    }){
+                        Text(key_char)
+                            .font(.system(size: font_size))
+                            .frame(maxWidth: .infinity, maxHeight: .infinity)
                     }
+                    .background(Color(red:0.8, green:0.9, blue:0.95))
+                    .foregroundColor(Color.black)
+                        .cornerRadius(10)
                 }
-                
             }
-        }//.frame(maxWidth: .infinity, maxHeight: .infinity)
+        }
     }
     
     func onckick(push_char: String)->Void{
